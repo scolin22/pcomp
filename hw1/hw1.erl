1a0c87bfb7a26249807eadebfa6b2c10d2a5afa0
Colin Stone <colin@bunnylaundering.com>
2016-01-12 01:04:14 -0800

    implemented prefix

--- hw1/hw1.erl
+++ hw1/hw1.erl
@@ -1,6 +1,17 @@
 -module(hw1).
 
--export([nthtail/2]).
+-export([nthtail/2, prefix/2]).
 
 nthtail(0, List) when is_list(List) -> List;
 nthtail(N, [_|T]) when (N > 0), (N =< length(T) + 1) -> nthtail(N-1, T).
+
+prefix([], List) when is_list(List) -> true;
+prefix(List, []) when is_list(List) -> false;
+prefix([H|T], List) when is_list(List) ->
+    [HList|TList] = List,
+    case H =:= HList of
+        true->
+            prefix(T, TList);
+        false->
+            false
+    end.
