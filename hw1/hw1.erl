fcdb7aa15de0d6aa3f6e8835316969a374ca5b20
Colin Stone <colin@bunnylaundering.com>
2016-01-13 17:01:00 -0800

    Implemented search.

--- hw1/hw1.erl
+++ hw1/hw1.erl
@@ -1,6 +1,6 @@
 -module(hw1).
 
--export([nthtail/2, prefix/2]).
+-export([nthtail/2, prefix/2, search/2]).
 
 nthtail(0, List) when is_list(List) -> List;
 nthtail(N, [_|T]) when (N > 0), (N =< length(T) + 1) -> nthtail(N-1, T).
@@ -15,3 +15,16 @@ prefix([H|T], List) when is_list(List) ->
         false->
             false
     end.
+
+search(List1, List2) when is_list(List1), is_list(List2) -> helper(List1, List2, [], 0).
+helper([], [], List0, N) -> lists:reverse([N+1 | List0]);
+helper(_, [], List0, _) -> lists:reverse(List0);
+helper(List1, List2, List0, N) ->
+    [_|T2] = List2,
+    case prefix(List1, List2) of
+        true->
+            helper(List1, T2, [N+1 | List0], N+1);
+        false->
+            helper(List1, T2, List0, N+1)
+    end.
+
