e5d32aa0d9502827547c35e41993567bd8abd36f
Colin Stone <colin@bunnylaundering.com>
2016-03-22 04:11:42 -0700

    wrap up mini3

--- mini3/mini3.erl
+++ mini3/mini3.erl
@@ -1,6 +1,7 @@
 -module(mini3).
 
 -export([is_lol/1, is_rect/1, transpose/1]).
+-export([stop_while_your_ahead/2]).
 
 is_lol([]) -> true;
 is_lol([H|T]) when is_list(H) ->
@@ -30,3 +31,50 @@ transpose(X) ->
         false ->
             erlang:error(bad_input)
     end.
+
+goodness([]) -> 0;
+goodness([good | Tl]) -> 1 + goodness(Tl);
+goodness([bad | Tl]) -> -1 + goodness(Tl);
+goodness([_ | Tl]) -> goodness(Tl);
+goodness(X) when not is_list(X) -> goodness([X]).
+
+stop_while_your_ahead(List, scan) ->
+    W = wtree:create(length(List)),
+    workers:update(W, raw_data, List),
+    stop_while_your_ahead_scan(W, 0, raw_data, cooked_data),
+    Res = lists:zip(workers:retrieve(W, cooked_data), lists:seq(1, length(List))),
+    Max = lists:foldl(fun({M, A}, {MAcc, _}) when
+    M > MAcc ->
+      {M, A};
+    (_, Acc) ->
+      Acc
+    end, {-1,0}, Res),
+    Max;
+stop_while_your_ahead(List, reduce) ->
+    W = wtree:create(length(List)),
+    workers:update(W, raw_data, List),
+    stop_while_your_ahead_reduce(W, raw_data).
+
+stop_while_your_ahead_scan(W, Acc, ListKey, GoodnessKey) ->
+    wtree:scan(W,
+        fun(ProcState) ->
+            goodness(wtree:get(ProcState, ListKey))
+        end,
+        fun(ProcState, AccIn) ->
+            wtree:put(ProcState, GoodnessKey, AccIn + goodness(wtree:get(ProcState, ListKey)))
+        end,
+        fun(Left, Right) ->
+            Left + Right
+        end,
+        Acc
+        ).
+
+stop_while_your_ahead_reduce(W, ListKey) ->
+    wtree:reduce(W,
+        fun(ProcState) ->
+            goodness(wtree:get(ProcState, ListKey))
+        end,
+        fun(Left, Right) ->
+            Left + Right
+        end
+        ).
